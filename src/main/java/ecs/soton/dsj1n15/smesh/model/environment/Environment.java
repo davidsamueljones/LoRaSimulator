3a6ef4592a80b4e04fe2c265e55415c06fcf5e1b
David Jones <davidsamueljones@icloud.com>
2019-03-21 23:59:10 +0000

    Commenting and tidyup of environment

--- src/main/java/ecs/soton/dsj1n15/smesh/model/environment/Environment.java
+++ src/main/java/ecs/soton/dsj1n15/smesh/model/environment/Environment.java
@@ -5,6 +5,7 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 import ecs.soton.dsj1n15.smesh.model.Radio;
 import ecs.soton.dsj1n15.smesh.model.Transmission;
+import ecs.soton.dsj1n15.smesh.model.Utilities;
 import ecs.soton.dsj1n15.smesh.model.propogation.PlainEarthPropagationModel;
 import ecs.soton.dsj1n15.smesh.model.propogation.PropagationModel;
 import math.geom2d.line.Line2D;
@@ -20,47 +21,23 @@ public class Environment {
   /** The current time in the environment */
   private long time;
 
-  public static double mw2dbm(double mw) {
-    return 10 * Math.log10(mw);
-  }
 
-  public static double dbm2mw(double dbm) {
-    return Math.pow(10, dbm / 10);
-  }
-
+  /**
+   * @return Reference to object
+   */
   public Set<EnvironmentObject> getEnvironmentObjects() {
     return objects;
   }
 
-  // public static void main(String[] args) {
-  //
-  //// Environment environment = new Environment();
-  //// double z = 0.25;
-  //// LoRaRadio sender = new LoRaRadio(1, LoRaCfg.getDefault());
-  //// sender.setZ(z);
-  //// environment.addNode(sender);
-  //// LoRaRadio receiver = new LoRaRadio(2, LoRaCfg.getDefault());
-  //// environment.addNode(receiver);
-  //// LoRaRadio interferer = new LoRaRadio(3, LoRaCfg.getDefault());
-  //// environment.addNode(interferer);
-  ////
-  ////
-  //// System.out.println(getReceivePower(se d));
-  //// Packet packet = new Packet(10);
-  ////
-  ////
-  ////
-  ////
-  //// environment.setTime(500);
-  //// Transmission tx = sender.send(packet);
-  //// for (int i=0; i < 20; i++) {
-  //// System.out.println(environment.getTransmissions().size());
-  //// environment.clearFinishedTransmissions();
-  //// long curTime = environment.getTime();
-  //// environment.setTime(curTime + 100);
-  //// }
-  // }
-
+  /**
+   * Calculate the signal to noise ratio for a signal sent from the given transmitter to the given
+   * receiver. A transmission must not necessarily be already occurring, the same value will be
+   * returned either way.
+   * 
+   * @param tx Transmitter
+   * @param rx Receiver
+   * @return The SNR in dBm kept within the range of the radio possibilities
+   */
   public double getReceiveSNR(Radio tx, Radio rx) {
     double strength = getReceivePower(tx, rx);
     double noise = getNoise(rx, tx.getCurrentTransmission());
@@ -68,13 +45,30 @@ public class Environment {
     return snr;
   }
 
+  /**
+   * Sum up the power in the listening channel; this is the sum of the noise floor, general
+   * receiever noise and all signals. See {@link #getNoise(Radio rx, Transmission target)}.
+   * 
+   * @param rx The receiver
+   * @return The RSSI in dBm
+   */
   public double getRSSI(Radio rx) {
     return getNoise(rx, null);
   }
 
+  /**
+   * Sum up all noise that the receiver will see. This is the thermal noise floor, the general
+   * receiver noise and noise from any signal that crosses into the listening channel. If the signal
+   * is considered to not interfere for other reasons such as orthogonality it is also ignored. If a
+   * target transmission is provided (not null), it will not be considered in any noise calculation.
+   * 
+   * @param rx The receiver
+   * @param target Signal to not count as noise
+   * @return The amount of noise in the channel in dBm
+   */
   public double getNoise(Radio rx, Transmission target) {
     // Calculate base noise level
-    double noise = dbm2mw(rx.getNoiseFloor());
+    double noise = Utilities.dbm2mw(rx.getNoiseFloor());
     // Sum the effect of any interfering transmissions on the target transmission
     for (Transmission interferer : getTransmissions()) {
       // Can't transmit and receive at the same time
@@ -85,24 +79,51 @@ public class Environment {
       if (target == interferer || !interferer.sender.canInterfere(rx)) {
         continue;
       }
-      double rp = dbm2mw(getReceivePower(interferer.sender, rx));
+      double rp = Utilities.dbm2mw(getReceivePower(interferer.sender, rx));
       noise += rp;
     }
-    return mw2dbm(noise);
+    return Utilities.mw2dbm(noise);
   }
 
+  /**
+   * Calculate how much power from a transmission will reach the receiver taking into account
+   * transmission power, gains and path loss.
+   * 
+   * @param tx Transmitter
+   * @param rx Receiver
+   * @return The signal power in dBm as seen by the receiver
+   */
   public double getReceivePower(Radio tx, Radio rx) {
     double txPow = tx.getTxPow() + tx.getAntennaGain() - tx.getCableLoss();
     double rxGain = rx.getAntennaGain() - rx.getCableLoss();
     return txPow - getAveragedPathLoss(tx, rx) + rxGain;
   }
 
+  /**
+   * Calculate the path loss after passing through objects in the environment. Averages two calls of
+   * {@link #getPathLoss(Radio tx, Radio rx)} with the input order reversed. This creates a value
+   * that is the average of path loss from transmitter to receiver and from receiver to transmitter.
+   * so that the value will be the same in both directions. This is not necessarily realistic but
+   * avoids unrealistic path differences.
+   * 
+   * @param tx A radio
+   * @param rx A radio
+   * @return The path loss in dbm in either direction
+   */
   public double getAveragedPathLoss(Radio tx, Radio rx) {
     double a = getPathLoss(tx, rx);
     double b = getPathLoss(rx, tx);
     return Math.min(a, b);
   }
 
+  /**
+   * Calculate the path loss after passing through objects in the environment. This is the sum of
+   * loss in free space and the sum of all propagation effects of said objects.
+   * 
+   * @param tx Transmitter
+   * @param rx Receiver
+   * @return The path loss in dBm from transmitter to receiver
+   */
   public double getPathLoss(Radio tx, Radio rx) {
     // Calculate the line of sight between transmitter and receive
     Line2D los = new Line2D(tx.getXY(), rx.getXY());
@@ -116,33 +137,22 @@ public class Environment {
       loss += object.getLOSPathLoss(tx, rx);
       loss += object.getProximityPathLoss(los);
     }
-
     return loss;
   }
 
-
-  public long getTime() {
-    return time;
-  }
-
-  public void setTime(long time) {
-    this.time = time;
-  }
-
+  /**
+   * @return All nodes in the environment
+   */
   public Set<Radio> getNodes() {
     return nodes;
   }
 
-  public Set<Transmission> getTransmissions() {
-    HashSet<Transmission> transmissions = new LinkedHashSet<>();
-    for (Radio radio : nodes) {
-      if (radio.getCurrentTransmission() != null) {
-        transmissions.add(radio.getCurrentTransmission());
-      }
-    }
-    return transmissions;
-  }
-
+  /**
+   * Add a new node to the environment, setting the nodes environment object automatically and
+   * removing it from its old environment if it exists.
+   * 
+   * @param radio Radio to add as node
+   */
   public void addNode(Radio radio) {
     Environment current = radio.getEnvironment();
     if (!this.equals(current)) {
@@ -156,9 +166,51 @@ public class Environment {
     }
   }
 
+  /**
+   * Remove node from the environment, unsetting radios environment object automatically.
+   * 
+   * @param radio Radio to remove
+   */
   public void removeNode(Radio radio) {
     radio.setEnvironment(null);
     nodes.remove(radio);
+  }
+
+  /**
+   * Check every node in the environment to see if they are transmitting, collating any found
+   * transmissions.
+   * 
+   * @return All transmissions in the environment
+   */
+  public Set<Transmission> getTransmissions() {
+    HashSet<Transmission> transmissions = new LinkedHashSet<>();
+    for (Radio radio : nodes) {
+      if (radio.getCurrentTransmission() != null) {
+        transmissions.add(radio.getCurrentTransmission());
+      }
+    }
+    return transmissions;
+  }
+
+  /**
+   * @return The current time (ms) in the environment
+   */
+  public long getTime() {
+    return time;
+  }
+
+  /**
+   * @param time The new time (ms) for the environment
+   */
+  public void setTime(long time) {
+    this.time = time;
+  }
+
+  /**
+   * @param time Value of time (ms) to add to the current time
+   */
+  public void addTime(long time) {
+    this.time += time;
   }
 
 }
