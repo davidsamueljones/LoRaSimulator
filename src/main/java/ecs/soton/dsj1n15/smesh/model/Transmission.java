74037de5f97e1e8566c62c10b3ac7db5b7e4d029
David Jones <davidsamueljones@icloud.com>
2019-03-21 22:53:01 +0000

    Fully defined a transmission

--- src/main/java/ecs/soton/dsj1n15/smesh/model/Transmission.java
+++ src/main/java/ecs/soton/dsj1n15/smesh/model/Transmission.java
@@ -2,8 +2,28 @@ package ecs.soton.dsj1n15.smesh.model;
 
 public class Transmission {
 
-  public Radio tx;
+  /** The radio that sent the transmission, use its configuration */
+  public final Radio sender;
+
+  /** The packet that is contained in the transmission */
+  public final Packet packet;
+
+  /** When the packet transmission started */
+  public final long startTime;
+
+  /** When the packet finishes */
+  public final long airtime;
   
-  public long startTime;
-  
+  /** When the packet finishes */
+  public final long endTime;
+
+
+  public Transmission(Radio sender, Packet packet, long startTime, long airtime) {
+    this.sender = sender;
+    this.packet = packet;
+    this.startTime = startTime;
+    this.airtime = airtime;
+    this.endTime = startTime + airtime;
+  }
+
 }
