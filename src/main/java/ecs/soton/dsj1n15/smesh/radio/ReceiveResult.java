f4f4ce092a8d250b727d316039f521d15964fd01
David Jones <davidsamueljones@icloud.com>
2019-04-06 16:35:17 +0100

    Modified receive results to wrap a transmission vs a packet

--- src/main/java/ecs/soton/dsj1n15/smesh/radio/ReceiveResult.java
+++ src/main/java/ecs/soton/dsj1n15/smesh/radio/ReceiveResult.java
@@ -3,7 +3,7 @@ package ecs.soton.dsj1n15.smesh.radio;
 public class ReceiveResult {
   public final Status status;
 
-  public final Packet packet;
+  public final Transmission transmission;
 
   public final long time;
 
@@ -11,26 +11,29 @@ public class ReceiveResult {
 
   public final double rssi;
 
-  private ReceiveResult(Status status, Packet packet, long time, double snr, double rssi) {
+  private ReceiveResult(Status status, Transmission transmission, long time, double snr,
+      double rssi) {
     this.status = status;
-    this.packet = packet;
+    this.transmission = transmission;
     this.time = time;
     this.snr = snr;
     this.rssi = rssi;
   }
- 
-  public static ReceiveResult getCollisionResult(long time, double snr, double rssi) {
-    return new ReceiveResult(Status.COLLISION, null, time, snr, rssi);
+
+  public static ReceiveResult getCollisionResult(Transmission transmission, long time) {
+    return new ReceiveResult(Status.COLLISION, transmission, time, 0, 0);
   }
   
-  public static ReceiveResult getCRCFailResult(long time, double snr, double rssi) {
-    return new ReceiveResult(Status.CRC_FAIL, null, time, snr, rssi);
+  public static ReceiveResult getCRCFailResult(Transmission transmission, long time, double snr,
+      double rssi) {
+    return new ReceiveResult(Status.CRC_FAIL, transmission, time, snr, rssi);
   }
-  
-  public static ReceiveResult getSuccessResult(Packet packet, long time, double snr, double rssi) {
-    return new ReceiveResult(Status.SUCCESS, null, time, snr, rssi);
+
+  public static ReceiveResult getSuccessResult(Transmission transmission, long time, double snr,
+      double rssi) {
+    return new ReceiveResult(Status.SUCCESS, transmission, time, snr, rssi);
   }
-  
+
   public enum Status {
     COLLISION, CRC_FAIL, SUCCESS
   }
